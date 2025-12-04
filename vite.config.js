014a4d2b0267b4598ae756da34c3dd6d33a694e6
VSah7079 <vk3815155@gmail.com>
2025-12-04 10:12:26 +0530

    chore: remove external configuration for react-icons to ensure default bundling

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-icons/fa',
-        'react-icons/hi'
-      ],
-      output: {
-        globals: {
-          'react-icons/fa': 'Fa',
-          'react-icons/hi': 'Hi'
-        }
-      }
-    }
-  }
+  // Keep default bundling so packages like `react-icons` are included
 })
